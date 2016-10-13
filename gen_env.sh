dc6ccdbc2b61fc57ad3191593db2f5262a474869
LAMBS Pierre-Antoine <pierreantoine.lambs@gmail.com>
2016-10-13 19:18:38 +0200

    update gen_env.sh
    Signed-off-by: LAMBS Pierre-Antoine <pierreantoine.lambs@gmail.com>

---
+++ gen_env.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+git_stm8flash="https://github.com/vdudouyt/stm8flash"
+git_src_test="https://github.com/vdudouyt/sdcc-examples-stm8.git"
+
+echo "All stm8 source are compiled with 'sdcc' please install it manualy..."
+sleep 2
+
+if [ ! -e ./example_src_stm8 ]; then
+	echo "==============================="
+	echo "||   Clone example src stm8  ||"
+	echo "==============================="
+	git clone ${git_src_test} ./example_src_stm8
+else
+	echo "example src for stm8 present don't clone..."
+fi
+
+if [ ! -e ./stm8flash ]; then
+	echo "==============================="
+	echo "||       Clone stm8flash     ||"
+	echo "==============================="
+	git clone ${git_stm8flash} ./stm8flash
+	echo "Build stm8flash..."
+	cd stm8flash
+	./configure
+	make
+	cd ..
+else
+	echo "stm8flash exist, don't clone it."
+fi
+
+exit 0
